3cad16ff0aa71925a1b3b5aa1d469d1deab18a86
Shubhang Sati <shubhang16198@iiitd.ac.in>
2018-05-06 14:18:18 +0530

    added multiply function and compiled

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 # command for step 1
-em++ $1 -s WASM=1 -o multiply.js
+# em++ $1 -s WASM=1 -o multiply.js
+
+
+# command for step 3
+em++ $1 -s WASM=1 -o multiply.js -s EXPORTED_FUNCTIONS='["_multiply"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap", "ccall"]'
--- multiply.cpp
+++ multiply.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+extern "C" int multiply(float x, float y) {
+    return x * y;
+}
+
 int main() {
     cout << "Initialized" << endl;
     return 0;
