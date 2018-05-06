dfe482e926ddef388fc2d6da1e1846217838f064
Shubhang Sati <shubhang16198@iiitd.ac.in>
2018-05-06 16:25:26 +0530

    updated cpp, build.sh and created multiply_in.js

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@
 
 
 # command for step 3
-em++ $1 -s WASM=1 -o multiply.js -s EXPORTED_FUNCTIONS='["_multiply"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap", "ccall"]'
+# em++ $1 -s WASM=1 -o multiply.js -s EXPORTED_FUNCTIONS='["_multiply"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap", "ccall"]'
+
+# command for step 4
+em++ $1 --js-library multiply_in.js -s WASM=1 -o multiply.js -s EXPORTED_FUNCTIONS='["_multiply"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap", "ccall"]' -s 
--- multiply.cpp
+++ multiply.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 
+extern "C" {
+    extern int multiply_in_js(float x, float y);
+}
+
 extern "C" int multiply(float x, float y) {
-    return x * y;
+    return multiply_in_js(x, y);
 }
 
 int main() {
