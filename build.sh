6c1c6e97321f85e4fb2edc2085042b4d989c99de
Shubhang Sati <shubhang16198@iiitd.ac.in>
2018-05-07 12:21:52 +0530

    updated build.sh and compiled

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@
 # em++ $1 -s WASM=1 -o multiply.js -s EXPORTED_FUNCTIONS='["_multiply"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap", "ccall"]'
 
 # command for step 4
-em++ $1 --js-library multiply_in.js -s WASM=1 -o multiply.js -s EXPORTED_FUNCTIONS='["_multiply"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap", "ccall"]'
+# em++ $1 --js-library multiply_in.js -s MODULARIZE=1 -s WASM=1 -o multiply.js -s EXPORTED_FUNCTIONS='["_multiply"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap", "ccall"]'
+
+
+# command for step 5
+em++ $1 --js-library multiply_in.js -s MODULARIZE=1 -s 'EXPORT_NAME="Multiply"' -s WASM=1 -o multiply.js -s EXPORTED_FUNCTIONS='["_multiply"]' -s EXTRA_EXPORTED_RUNTIME_METHODS='["cwrap", "ccall"]'
